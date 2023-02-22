205d9e6fdd33d9635ea8ff54c9c9aef8d806fc7d
Bui Tran Phuong Thao <121841993+thaobtpgcc210031@users.noreply.github.com>
2023-02-22 15:33:17 +0700

    add category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    /**
-     * @Route("/category", name="app_category")
-     */
-    public function index(): Response
+    private CategoryRepository $repo;
+    public function __construct(CategoryRepository $repo)
     {
+       $this->repo = $repo;
+    }
+    /**
+     * @Route("/categoryy", name="category_show")
+     */
+    public function allCategoryAction(): Response
+    {
+        $category = $this->repo->findAll();
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'category'=>$category
         ]);
     }
+
+    //ADMIN
+    //ADD Category
+    /**
+     * @Route("/addcate", name="category_create")
+     */
+    public function addCategoryAction(CategoryRepository $repo, Request $req): Response
+    {
+        $cate = new Category();
+        $form = $this->createForm(CategoryType::class, $cate);
+
+        $form->handleRequest($req);
+        if($form->isSubmitted()&& $form->isValid())
+        {
+            $repo->add($cate, true);
+            return $this->redirectToRoute('category_show', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('category/form.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+
 }
