1a44b965db5ba19b9c43835e49d78635f3015c9e
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2023-02-24 00:50:38 +0700

    update home crt

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +18,23 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+
+    //search controller
+    /**
+     * @Route("/search", name="search", methods={"GET"})
+     */
+    public function actionSearch(Request $req, ProductRepository $repo, CategoryRepository $repo1): Response
+    {
+        $br = $repo1->findAll();
+        $sName = $req->query->get("search");
+        $product = $repo->findBysearchproduct($sName);
+        return $this->render('search/index.html.twig', [
+            'pro' => $product,
+            'brand' => $br
         ]);
     }
 }
